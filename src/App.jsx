ac9fcce2b4634581efe4c73fc443bb3b45df22f7
pinyi <s110919041@stu.ntue.edu.tw>
2023-04-09 14:38:56 +0800

    vercel 測試

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import Product from './pages/Product'
 import Category from './pages/Category'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HelmetProvider } from "react-helmet-async"
-import store from './redux/store';
+// import store from './redux/store';
 import { Provider } from 'react-redux';
 import { ConfigProvider } from 'antd';
 import { darkTheme, lightTheme } from './theme';
 import Router from './Router';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 function App() {
   return (
 
-    <Provider store={store} >
-      <Router/>
-    </Provider>
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router />
+    </PersistGate>
+  </Provider>
 
 
   )
