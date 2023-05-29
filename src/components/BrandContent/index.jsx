af50aa108c9b00079cf89b11d4c2791c35cb67de
ho-chi <s110919047@stu.ntue.edu.tw>
2023-05-30 00:42:19 +0800

    modufy Brand Page css

--- src/components/BrandContent/index.jsx
+++ src/components/BrandContent/index.jsx
@@ -36,7 +36,7 @@ export default function BrandContent({ title }) {
                 <Row className={styles.RowCol}>
                     <Col className={styles.RowCol}>
                         <div className={styles.boxstyle}>
-                            <motion.h1
+                            {/* <motion.h1
                                 className={styles.title}
                                 variants={typingContainer}
                                 initial="hidden"
@@ -47,11 +47,16 @@ export default function BrandContent({ title }) {
                                         {word}
                                     </motion.span>
                                 ))}
-                            </motion.h1>
+                            </motion.h1> */}
                             <img
                                 className={styles.ns}
                                 src="/images/login_logo.png"
                                 alt="news1" />
+                            <p className={styles.description}>
+                                「偶素甜點」集結了多間知名素甜點店，作為整合通路商，為素食者打造專屬的甜點天地 ，我們與多個品牌合作，進行整合與分類以素別、
+                                種類、品牌等多面向切入，打造出素食新意象，精緻化的同時拉近大家與素食的距離。
+                            </p>
+
                         </div>
 
                     </Col>
